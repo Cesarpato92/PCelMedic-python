fa4b2c92b622da6c9cd3bf3e4abd1538154202ad
Cesarpato92 <parcerazo@gmail.com>
2025-11-26 08:01:52 -0500

    Modifiacion de validacion de campos en logicaDispositivo

--- Logica/LogicaDispositivo.py
+++ Logica/LogicaDispositivo.py
@@ -10,20 +10,10 @@ class LogicaDispositivo:
     en cada metodo realizamos las validaciones o reglas de negocio necesarias antes o despues de llamar al DAO
     Verificamos que los datos sean correctos antes de llamar al DAO, etc."""
     def agregar_dispositivo(self, modelo_dispositivo):
-        # Validaciones básicas
-        if not modelo_dispositivo.marca or modelo_dispositivo.marca.strip() == "":
-            messagebox.showerror("Error", "La marca es obligatoria.")
-            return None
-        if not modelo_dispositivo.tipo_reparacion or modelo_dispositivo.tipo_reparacion.strip() == "":
-            messagebox.showerror("Error", "El tipo de reparación es obligatorio.")
-            return None
-        if modelo_dispositivo.tipo_password in ["PIN", "Contraseña", "Patrón"]:
-            if not modelo_dispositivo.password or modelo_dispositivo.password.strip() == "":
-                messagebox.showerror("Error", f"La contraseña para el tipo {modelo_dispositivo.tipo_password} es obligatoria.")
-                return None
         
-        # Llamamos al DAO para agregar el dispositivo luego de pasar las validaciones
-        return self.dispositivo_dao.agregar_dispositivo(modelo_dispositivo)
+        if self.validacion_datos(modelo_dispositivo):
+            # Llamamos al DAO para agregar el dispositivo luego de pasar las validaciones
+            return self.dispositivo_dao.agregar_dispositivo(modelo_dispositivo)
 
     def obtener_dispositivo_por_id(self, modelo_dispositivo):
         return self.dispositivo_dao.obtener_dispositivo_por_id(modelo_dispositivo)
@@ -31,5 +21,19 @@ class LogicaDispositivo:
     def actualizar_dispositivo(self, modelo_dispositivo):
         self.dispositivo_dao.actualizar_dispositivo(modelo_dispositivo)
     
-
-    
+    def validacion_datos(self, modelo_dispositivo):
+        # Validaciones básicas
+        if not modelo_dispositivo.marca or modelo_dispositivo.marca.strip() == "":
+            messagebox.showerror("Error", "La marca es obligatoria.")
+            return False
+        if not modelo_dispositivo.modelo  or modelo_dispositivo.modelo.strip() == "":
+            messagebox.showerror("Error", "El modelo es obligatoria.")
+            return False
+        if not modelo_dispositivo.tipo_reparacion or modelo_dispositivo.tipo_reparacion.strip() == "":
+            messagebox.showerror("Error", "El tipo de reparación es obligatorio.")
+            return False
+        if modelo_dispositivo.tipo_password in ["PIN", "Contraseña", "Patrón"]:
+            if not modelo_dispositivo.password or modelo_dispositivo.password.strip() == "":
+                messagebox.showerror("Error", f"La contraseña para el tipo {modelo_dispositivo.tipo_password} es obligatoria.")
+                return False
+        return True
