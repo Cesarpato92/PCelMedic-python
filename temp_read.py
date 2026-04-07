c57d3d38edeadb698747d2cc774ad908b2c78d6b
Cesarpato92 <parcerazo@gmail.com>
2026-04-06 23:13:00 -0500

    feat: borrar archivos de prueba

--- temp_read.py
+++
@@ -1,3 +0,0 @@
-with open('requirements.txt', 'r', encoding='utf-16') as f:
-    content = f.read()
-    print(repr(content))
--- test_conexion.py
+++
@@ -1,9 +0,0 @@
-from Utilidades.Conexion import Conexion
-
-# Test the connection
-conn = Conexion.get_conexion()
-if conn:
-    print("Connection successful")
-    Conexion.desconectar()
-else:
-    print("Connection failed")
